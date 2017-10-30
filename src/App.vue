731fa7405e9124ea7965f8a4ed8ce27758c5c940
tianyu <344763278@qq.com>
2017-10-30 09:03:39 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    remInit(doc, win) {
+      let docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function() {
+          let clientWidth = docEl.clientWidth
+          if (!clientWidth) return
+          if (clientWidth >= 740) {
+            clientWidth = 740
+          }
+          if (clientWidth <= 320) {
+            clientWidth = 320
+          }
+          docEl.style.fontSize = 100 * (clientWidth / 640) + 'px'
+        }
+      if (!doc.addEventListener) return
+      recalc();
+      win.addEventListener(resizeEvt, recalc, false)
+    }
+
+  },
+  mounted() {
+    this.remInit(document, window)
+  }
+}
+
+</script>
+<style scoped lang="scss">
+#app {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="index-wrap">
+    <div class="header">顶部导航</div>
+    <div class="main"></div>
+    <div class="footer">底部导航</div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.index-wrap {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  position: relative;
+  .header {
+    position: absolute;
+    left: 0;
+    top: 0;
+    height: 0.63rem;
+    background: #FE5400;
+    font-size: 0.2rem;
+    width: 6.4rem;
+    text-align: center;
+    line-height: 0.63rem;
+  }
+  .footer {
+    position: absolute;
+    width: 6.4rem;
+    left: 0;
+    bottom: 0;
+    height: 0.63rem;
+    background: #FE5400;
+    font-size: 0.2rem;
+    text-align: center;
+    line-height: 0.63rem;
+  }
+  .main {
+    width: 6.4rem;
+    height: 20rem;
+    font-size: 0.2rem;
+    padding: 0.63rem 0; // overflow: auto;
+  }
+  .main {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0.63rem;
+    bottom: 0.63rem; // width: 6.4rem;
+    // height: 20rem;
+    font-size: 0.2rem; // padding: 0.63rem 0;
+    overflow: auto;
+  }
+}
+
+</style>
